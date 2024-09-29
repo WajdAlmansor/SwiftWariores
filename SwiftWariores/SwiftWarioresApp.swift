4a1512dcc9984d06a2465a5bded24c0db71618f9
WajdButterfly <wajd2828@gmail.com>
2024-09-29 14:48:54 +0300

    Initial Commit

---
+++ SwiftWariores/SwiftWarioresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWarioresApp.swift
+//  SwiftWariores
+//
+//  Created by Wajd on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWarioresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
