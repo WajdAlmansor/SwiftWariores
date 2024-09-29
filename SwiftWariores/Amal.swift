db4b6bf8c7cc43047dfd99bdb0f5436ccbd3db91
amalalm <amalmoh.mtr@gmail.com>
2024-09-29 16:47:37 +0300

    Amal

--- SwiftWariores/Amal.swift
+++ SwiftWariores/Amal.swift
@@ -8,29 +8,30 @@
 import SwiftUI
 
 struct Amal: View {
-    var body: some View {
-        VStack{
-            Image("Raneem")
-                .resizable()
-                .scaledToFit()
-                .clipShape(/@START_MENU_TOKEN@/Circle()/@END_MENU_TOKEN@/)
-                .overlay(Circle().stroke(Color.gray, lineWidth: 4))
-            
-            Text("Raneem Alomair")
-                .font(.title)
-                .fontWeight(/@START_MENU_TOKEN@/.bold/@END_MENU_TOKEN@/)
-                .foregroundColor(Color.black)
-            
-                .padding(.bottom)
-                Text("My name is Raneem, Im 20 years old and im an IT studend at IMAMU ")
-            
-               
-            
-            
+   
+        var body: some View {
+            VStack{
+                Image("profileImage")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+                
+                Text("Amal Almutairi")
+                    .font(.title)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.black)
+                
+                    .padding(.bottom)
+                    Text("My name is amal, Im 22 years old and im an IT studend at IMAMU ")
+                
+                   
+                
+                
+            }
         }
     }
-}
 
-#Preview {
-    Raneem()
-}
+    #Preview {
+        Amal()
+    }
